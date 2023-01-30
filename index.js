ef80432faec4b4aa2b4d2a977c37894494cd7374
Nera26 <106533634+Nera26@users.noreply.github.com>
2023-01-30 18:09:01 +0800

    gg
    gg

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+       var dropdown = document.getElementById("navbar-dropdown");
+       if (dropdown.style.display === "block") {
+         dropdown.style.display = "none";
+       } else {
+         dropdown.style.display = "block";
+       }
+     }
